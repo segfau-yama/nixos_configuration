7d0a07abb22093fd80a75d3acd6d7ed58e0c7018
segfau-yama <suiki547@gmail.com>
2026-05-23 14:13:18 +0900

    デスクトップエラー修正

--- modules/users/admin/nixos.nix
+++ modules/users/admin/nixos.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ inputs, ... }:
 let
   username = "admin";
 in
 {
-  # admin: 管理者ユーザー。Home Manager は持たない。
+  # admin: 管理者ユーザー。GUI ホストでは niri がユーザー別 config.kdl を読むため、
+  # 最小限の Home Manager desktop 設定を持たせる。
   flake.modules.nixos."${username}" = { pkgs, ... }: {
     users.users."${username}" = {
       isNormalUser = true;
@@ -20,5 +21,21 @@ in
     };
 
     programs.zsh.enable = true;
+
+    home-manager.users."${username}" = {
+      imports = [ inputs.self.modules.homeManager."${username}" ];
+    };
+  };
+
+  flake.modules.homeManager."${username}" = { ... }: {
+    imports = [
+      inputs.self.modules.homeManager.desktop
+    ];
+
+    home.username      = "${username}";
+    home.homeDirectory = "/home/${username}";
+    home.stateVersion  = "25.05";
+
+    programs.home-manager.enable = true;
   };
 }
